291f78b8d657e59b37c7c8a9e948c035d3c84ac9
CodyAdam <cody.adam@laposte.net>
2020-12-15 20:57:19 +0100

    Day 14 part 2 finished

--- day14.2.py
+++ day14.2.py
@@ -10,15 +10,39 @@ def dec(str):
     return int(str, 2)
 
 
-def applyMask(value):
+def applyMask(bina):
     for i in range(len(mask)):
-        if(mask[i] == "0"):
+        if (mask[i] == "0"):
             continue
-        value = value[:i] + mask[i] + (value[i+1:] if i+1 < len(mask) else "")
-    return value
+        bina = bina[:i] + mask[i] + (bina[i + 1:] if i + 1 < len(mask) else "")
+    return bina
 
 
-def putAtAddress(value):
+def xCount(bina):
+    sum = 0
+    for char in bina:
+        if char == "X":
+            sum += 1
+    return sum
+
+
+def putAtAddress(addressBin, toPut):
+    n = xCount(addressBin)
+    for i in range(pow(2, n)):
+        x = decimalToBinary(i)
+        xReplacement = ["0"] * n
+        for j in range(len(x)):
+            xReplacement[j - len(x)] = x[j]
+        print(xReplacement)
+
+        xCounter = 0
+        tempAdd = list(addressBin)
+        for j in range(len(tempAdd)):
+            if tempAdd[j] == "X":
+                tempAdd[j] = xReplacement[xCounter]
+                xCounter += 1
+        newAddress = dec("".join(tempAdd))
+        mem[newAddress] = int(toPut)
 
 
 mem = dict()
@@ -28,13 +52,15 @@ for i in range(len(IN)):
         mask = IN[i].split(" = ")[1]
         continue
     address, value = re.findall("\d+", IN[i])
-    value = str(decimalToBinary(int(value)))
-    binaire = "000000000000000000000000000000000000"[
-        0:36-len(value)] + value
-    mem[address] = dec(applyMask(binaire))
+
+    addBin = str(decimalToBinary(int(address)))
+    binaire = applyMask("000000000000000000000000000000000000" [0:36 -
+                                                                len(addBin)] +
+                        addBin)
+    putAtAddress(binaire, value)
 
 print(mem)
-# sum = 0
-# for add in mem:
-#     sum += mem(add)
-# print(sum)
+sum = 0
+for add in mem:
+    sum += mem[add]
+print(sum)
